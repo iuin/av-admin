f57aa3130e91474c0ccb63013bc2d2ec74d957f3
Fulin <fulin.zhang@emc.com>
2018-10-31 17:10:46 +0800

    build structure

--- src/app/layout/default/default.component.ts
+++ src/app/layout/default/default.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import {
+    Router,
+    NavigationStart,
+    NavigationEnd,
+    RouteConfigLoadStart,
+    NavigationError,
+    NavigationCancel,
+} from '@angular/router';
 
 @Component({
     selector: 'layout-default',
@@ -7,7 +15,26 @@ import { environment } from '../../../environments/environment';
     styleUrls: ['./default.component.scss']
 })
 export class LayoutDefaultComponent implements OnInit {
-    constructor() { }
+
+    isFetching = false;
+
+    constructor(router: Router) {
+        router.events.subscribe(evt => {
+            if (!this.isFetching && (evt instanceof RouteConfigLoadStart || evt instanceof NavigationStart)) {
+              this.isFetching = true;
+            }
+            if (evt instanceof NavigationError || evt instanceof NavigationCancel) {
+              this.isFetching = false;
+              return;
+            }
+            if (!(evt instanceof NavigationEnd)) {
+              return;
+            }
+            setTimeout(() => {
+                this.isFetching = false;
+            }, 1500);
+          });
+     }
 
     ngOnInit(): void { }
 }
